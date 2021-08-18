4224e9c358b1973af66a379749393bca31289052
dave franks <flashman123@fastmail.fm>
2021-08-18 10:56:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+
+// docker run --rm --name master26-mysql -dp 3306:3306 -v mysql-data-master26:/var/lib/mysql 
+// -v mysql-config-master26:/etc mysql/mysql-server:latest
+
+require("dotenv").config();
